70d3cc53845fecebe2759f2269e368e504b85821
salimHarthi <salim.h.harthi@gmail.com>
2023-05-28 15:46:32 +0400

    you can edit a singel card

--- components/imageNode.jsx
+++ components/imageNode.jsx
@@ -1,16 +1,48 @@
-import { memo } from 'react';
+import { memo, useState, useEffect } from 'react';
 import { Handle, Position, useReactFlow, useNodeId } from 'reactflow';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { Card } from 'antd';
+import { EditOutlined, DeleteOutlined, SaveOutlined } from '@ant-design/icons';
+import { Card, Form, DatePicker, Input } from 'antd';
 import Image from 'next/image';
 import { cardWidth } from '@/constent';
+import dayjs from 'dayjs';
 const { Meta } = Card;
 export default memo(({ data: { birthday, name, image }, isConnectable }) => {
   const nodeId = useNodeId();
-  const { deleteElements, getNode } = useReactFlow();
+  const [form] = Form.useForm();
+  const [showForm, setShowForm] = useState(false);
+  const { deleteElements, getNode, setNodes } = useReactFlow();
   const handleDeleteClick = () => {
     deleteElements({ nodes: [getNode(nodeId)] });
   };
+  const onFinish = (value) => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === nodeId) {
+          node.data = {
+            ...node.data,
+            name: value?.fullName,
+            birthday: value?.birthDay?.format('YYYY/MM/DD'),
+          };
+        }
+
+        return node;
+      })
+    );
+    setShowForm(false);
+  };
+  const editSave = showForm ? (
+    <SaveOutlined key='save' onClick={form.submit} />
+  ) : (
+    <EditOutlined
+      key='edit'
+      onClick={() => {
+        setShowForm(true);
+      }}
+    />
+  );
+  useEffect(() => {
+    form.resetFields();
+  }, [birthday, name]);
   return (
     <>
       <Handle
@@ -34,11 +66,51 @@ export default memo(({ data: { birthday, name, image }, isConnectable }) => {
           />
         }
         actions={[
-          <EditOutlined key='edit' />,
+          editSave,
           <DeleteOutlined key='delete' onClick={handleDeleteClick} />,
         ]}
       >
-        <Meta className='text-center' title={name} description={birthday} />
+        {showForm ? (
+          <Form
+            form={form}
+            onFinish={onFinish}
+            initialValues={{
+              fullName: name,
+              birthDay: dayjs(birthday),
+            }}
+            wrapperCol={{
+              span: 23,
+            }}
+            layout='horizontal'
+            style={{
+              marginTop: 20,
+              maxWidth: 600,
+            }}
+          >
+            <Form.Item
+              label='Full Name'
+              name='fullName'
+              labelCol={{ span: 24 }}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter Full name',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label='Date of birth'
+              name='birthDay'
+              labelCol={{ span: 24 }}
+            >
+              <DatePicker />
+            </Form.Item>
+          </Form>
+        ) : (
+          <Meta className='text-center' title={name} description={birthday} />
+        )}
       </Card>
 
       <Handle
